cd2789aa934193438b1b40b94ed9aa98513b96ff
Bang Hyechan <38344292+momo1108@users.noreply.github.com>
2019-08-28 17:47:29 +0900

    stack5

---
+++ programmers/stack&que5.js
@@ -0,0 +1,12 @@
+function solution(progresses, speeds) {
+    var answer = [];
+    var temp = [];
+    while(1){
+        progresses.forEach((e,i) => {
+            e += speeds[i];
+            if(e>=100)
+        });
+        
+    }
+    return answer;
+}
